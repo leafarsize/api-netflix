72c33fd0904d10ba81841a2d8dd06bda74a664d3
leafarsize <152042162+leafarsize@users.noreply.github.com>
2026-07-21 10:27:08 -0700

    nuevo

--- ClienteNetflix/app.js
+++ ClienteNetflix/app.js
@@ -1,39 +1,4 @@
 
-const API_URL = "https://api-netflix-whma.vercel.app/";
-
-// Obtener películas
-async function obtenerPeliculas() {
-
-    const respuesta = await fetch(`${API_URL}/peliculas`);
-
-    if (!respuesta.ok) {
-        throw new Error("Error al consultar las películas");
-    }
-
-    return await respuesta.json();
-
-}
-
-// Agregar película
-async function agregarPelicula(pelicula) {
-
-    const respuesta = await fetch(`${API_URL}/peliculas`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(pelicula)
-    });
-
-    if (!respuesta.ok) {
-        throw new Error("Error al guardar la película");
-    }
-
-    return await respuesta.json();
-
-}
-
-
 const formulario = document.getElementById("formulario");
 
 const titulo = document.getElementById("titulo");
